9c28bf0fc95e0629ba369f89899c5facb8f9e1e6
Zachary Stevens <zts@cryptocracy.com>
2011-08-03 12:56:19 +0100

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "zts@cryptocracy.com"
 license          "Apache v2.0"
 description      "Installs/Configures mcollective"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.8"
+version          "0.0.9"
 
 supports 'ubuntu'
 supports 'centos'
