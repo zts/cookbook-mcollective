83a82fdc4883c8be26deaaf41dd34a5745913d37
Zachary Stevens <zts@cryptocracy.com>
2014-01-11 20:11:03 +0000

    Make rabbitmq integration test pass.

---
+++ test/fixtures/cookbooks/mcollective_test/recipes/rabbitmq.rb
@@ -0,0 +1,34 @@
+# Install common stuff
+include_recipe "mcollective_test::default"
+
+# Install rabbitmq
+package "logrotate"
+
+node.set['rabbitmq']['stomp'] = true
+node.set['rabbitmq']['stomp_port'] = node['mcollective']['stomp']['port']
+#include_recipe "rabbitmq::mgmt_console"
+
+# Configure rabbitmq for mcollective
+rabbitmq_user node['mcollective']['stomp']['username'] do
+  password node['mcollective']['stomp']['password']
+  action :add
+end
+rabbitmq_user node['mcollective']['stomp']['username'] do
+  vhost node['mcollective']['rabbitmq']['vhost']
+  permissions ".* .* .*"
+  action :set_permissions
+end
+
+remote_file "/usr/bin/rabbitmqadmin" do
+  source "http://127.0.0.1:15672/cli/rabbitmqadmin"
+  mode "755"
+  action :create
+end
+bash "declare mcollective exchanges" do
+  user "root"
+  code <<-EOF
+/usr/bin/rabbitmqadmin declare exchange name=mcollective_broadcast type=topic
+/usr/bin/rabbitmqadmin declare exchange name=mcollective_directed type=direct
+  EOF
+  action :run
+end
