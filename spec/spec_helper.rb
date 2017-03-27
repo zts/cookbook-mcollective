0f78c4a132091bac29d302188afb7e9dc4a8cc02
Zachary Stevens <zts@elastera.com>
2017-03-27 21:45:58 +0100

    Pretty output for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,20 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'yarjuf'
+
+RSpec.configure do |config|
+  # Specify the Chef log_level (default: :warn)
+  # config.log_level = :debug
+
+  # Specify the operating platform to mock Ohai data from
+  config.platform = 'centos'
+
+  # Specify the operating version to mock Ohai data from
+  config.version = '6.4'
+
+  # Use color output for RSpec
+  config.color = true
+
+  # Use documentation output formatter
+  config.formatter = :documentation
+end
